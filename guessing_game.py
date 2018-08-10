3818e9127427d6df5b72108781ae3ddb34b06db4
Daniel <dpeterson@basecampcodingacademy.org>
2018-08-10 13:05:27 -0500

    broke it into smaller functions

--- guessing_game.py
+++ guessing_game.py
@@ -3,16 +3,45 @@ from termcolor import colored
 import time
 
 
-def main():
-    print(colored('Welcome to the guessing game!', 'yellow'))
-    print(colored('Please guess a number from 1 - 100!', 'yellow'))
-    random_num = random.randint(1, 100)
+def redemption_round(guesses_remaining_redemption_round):
+    random_num2 = random.randint(1, 25)
+    while True:
+        num = int(
+            input(
+                colored('Redemption Round...What is your guess 1-25: ',
+                        'yellow')))
+        if num > 25 or num < 0:
+            print(colored('Number must be between 1 and 25', 'red'))
+            print()
+            continue
+        guesses_remaining_redemption_round = guesses_remaining_redemption_round - 1
+        if num == random_num2:
+            print()
+            print(colored('****SUCCESS!!****', 'blue'))
+            break
 
-    guesses_remaining = 6
+        if guesses_remaining_redemption_round == 0:
+            print(colored('Womp womp, womp, wooooommmmmp :-( !!', 'red'))
+            print()
+            print('\tThe winning number was: ', random_num2)
+            break
+
+        else:
+            if num > random_num2:
+                print(colored('REALLY.... guess lower!', 'cyan'))
+                print()
+            elif num < random_num2:
+                print(colored('Guess a little bit higher my guy!', 'cyan'))
+                print()
+                continue
+
+
+def first_round(guesses_remaining):
+    random_num = random.randint(1, 100)
     while True:
         user_num = int(input('Guess a number: '))
         if user_num > 100 or user_num < 0:
-            print('Number must be between 1 and 100')
+            print(colored('\nNumber must be between 1 and 100', 'red'))
             continue
         guesses_remaining = guesses_remaining - 1
         if user_num == random_num:
@@ -24,41 +53,35 @@ def main():
         else:
             if random_num > user_num:
                 print(colored("Choose Higher", 'cyan'))
+                print()
             elif random_num < user_num:
                 print(colored("Choose Lower", 'cyan'))
+                print()
 
         if guesses_remaining == 0:
+            print('\tThe winning number was: ', random_num)
             print(colored('Sorry.... Ran out of tries', 'red'))
-            print('The winning number was: ', random_num)
-            print('BUUUUUUUUUUUUUUT.......')
-            time.sleep(3)
+            print()
+            print(colored('BUUUUUUUUUUUUUUT.......', 'green'))
+            print()
+            time.sleep(2)
             break
 
-    random_num2 = random.randint(1, 25)
-    guesses_remaining_redemption_round = 4
-    while True:
 
-        num = int(
-            input(
-                colored('Redemption Round...What is your guess 1-25: ',
-                        'yellow')))
+def printing_helper():
+    print(colored('\tGuessing Game', attrs=['bold']))
+    print()
+    print(colored('*****Hit Enter to began*****', 'red'))
+    input()
+    print(colored('Please guess a number from 1 - 100!', 'yellow'))
 
-        guesses_remaining_redemption_round = guesses_remaining_redemption_round - 1
 
-        if num == random_num2:
-            print('SUCCESS!!')
-            break
-
-        if guesses_remaining_redemption_round == 0:
-            print(colored('Womp, womp, womp, wooooommmmmp :-( !!', 'red'))
-            print('The winning number was: ', random_num2)
-            break
-
-        else:
-            if num > random_num2:
-                print(colored('REALLY.... guess lower!', 'cyan'))
-            elif num < random_num2:
-                print(colored('Guess a little bit higher my guy!', 'cyan'))
+def main():
+    printing_helper()
+    guesses_remaining = 5
+    first_round(guesses_remaining)
+    guesses_remaining_redemption_round = 3
+    redemption_round(guesses_remaining_redemption_round)
 
 
 if __name__ == '__main__':
