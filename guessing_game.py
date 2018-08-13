56a6ab770e0ed5ae3e12c9bd0262f8bd2594daf6
Daniel <dpeterson@basecampcodingacademy.org>
2018-08-13 13:38:06 -0500

    made it more complicated by adding choice and then making it long

--- guessing_game.py
+++ guessing_game.py
@@ -9,12 +9,11 @@ def welcome():
     print()
     print(colored('*****Hit Enter to began*****', 'red'))
     input()
-    print(colored('Please guess a number from 1 - 100!', 'yellow'))
 
 
 def redemption_round(guesses_remaining_redemption_round):
-    random_num2 = random.randint(1, 25)
     computer_random_num2 = random.randint(1, 25)
+    random_num2 = random.randint(1, 25)
     print(colored('Redemption Round guess a number from 1 to 25', 'blue'))
     time.sleep(1)
     print()
@@ -52,9 +51,43 @@ def redemption_round(guesses_remaining_redemption_round):
                     continue
 
 
+def different_game(advanced_guesses_remaining):
+    choose = input(
+        'Would you like the Standard Version(1) or the Advanced Version(2)? ')
+    if choose == '1':
+        guesses_remaining = 5
+        print(colored('Please guess a number from 1 - 100!', 'yellow'))
+        first_round(guesses_remaining)
+        print('Still between 1-100')
+    else:
+        advanced_random_num = random.randint(1, 200)
+        while True:
+            print('\nChoose a number between 1 and 200!')
+            advanced_user_num = int(input('Choose your number!! '))
+            if advanced_user_num > 200 or advanced_user_num < 0:
+                print(colored('\nNumber must be between 1 and 200', 'red'))
+                continue
+            advanced_guesses_remaining = advanced_guesses_remaining - 1
+            if advanced_user_num == advanced_random_num:
+                print(colored('Bet that... You Got It!!!', 'green'))
+                exit()
+            else:
+                if advanced_random_num > advanced_user_num:
+                    print(colored("Up it up a little bit!", 'cyan'))
+                    print()
+                elif advanced_random_num < advanced_user_num:
+                    print(colored("PIPE Down my guy!", 'cyan'))
+                    print()
+            if advanced_guesses_remaining == 0:
+                print('\tThe winning number was: ', advanced_random_num)
+                print(colored('Sorry.... Ran out of tries', 'red'))
+                print('Now Choose a number between 1-100')
+                break
+
+
 def first_round(guesses_remaining):
-    random_num = random.randint(1, 100)
     computer_random_num = random.randint(1, 100)
+    random_num = random.randint(1, 100)
     while True:
         if computer_random_num == random_num:
             print('Computer Got the DUUB!')
@@ -90,7 +123,9 @@ def first_round(guesses_remaining):
 
 def main():
     welcome()
-    guesses_remaining = 5
+    advanced_guesses_remaining = 8
+    different_game(advanced_guesses_remaining)
+    guesses_remaining = 4
     first_round(guesses_remaining)
     guesses_remaining_redemption_round = 3
     redemption_round(guesses_remaining_redemption_round)
